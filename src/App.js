a733ad2f1415a9d9465a29a1f3cb5c6be36257c3
Nikita Kozlov <yellowswordy@yandex.com>
2019-10-18 20:15:12 -0700

    Create small components from homepage. Use separation of conserns

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Homepage from "./homepage.component";
+import Homepage from "./components/pages/homepage.component";
 
 function App() {
   return (
---
+++ src/components/pages/homepage.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Directory from "../directory/directory.component";
+
+import './homepage.styles.scss'
+
+const Homepage = () => (
+    <div className='homepage'>
+        <Directory/>
+    </div>
+);
+
+export default Homepage;
